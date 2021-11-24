7b4f201b7f90cd9fd64c01c42b41e711a3c6001b
satonohime <nananariru@gmail.com>
2021-11-24 11:32:06 -0800

    added contact data for script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const contactList = [  
+	{ 
+		name: "Oliver Queen", 
+		phone: "778-555-1234", 
+		address: "101 Main St, Star City, USA",    
+		email: "greenarrow@watchtower.com",  
+	},   
+	{    
+		name: "Jessica Cruz",    
+		phone: "123-555-5555",    
+		address: "Portland Oregon",    
+		email: "greenlantern@watchtower.com",  
+	}
+]
+
 function cleanUpIndex() {
     let mainClassElem = document.querySelector('.main')
     
